78654a04cc0471f7039e83f43c284e6e41b85fe5
Thomas Schucker <thomas.schucker@decatechnologies.com>
2016-02-12 14:29:17 -0700

    Initial Commit

---
+++ Raytracer3D/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Raytracer3D
+//
+//  Created by Thomas Schucker on 2/12/16.
+//  Copyright © 2016 BWAC. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
