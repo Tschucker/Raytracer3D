7fbc8116f91d5d8109ab28032ef999288c9e4e48
Thomas Schucker <thomas.schucker@decatechnologies.com>
2016-02-25 17:20:25 -0700

    add scene descriptor object

---
+++ Raytracer3D/Scene.cpp
@@ -0,0 +1,9 @@
+//
+//  Scene.cpp
+//  Raytracer3D
+//
+//  Created by Thomas Schucker on 2/25/16.
+//  Copyright © 2016 BWAC. All rights reserved.
+//
+
+#include "Scene.hpp"
---
+++ Raytracer3D/Scene.hpp
@@ -0,0 +1,17 @@
+//
+//  Scene.hpp
+//  Raytracer3D
+//
+//  Created by Thomas Schucker on 2/25/16.
+//  Copyright © 2016 BWAC. All rights reserved.
+//
+
+#ifndef Scene_hpp
+#define Scene_hpp
+
+#include <stdio.h>
+#include "Rotor.hpp"
+#include "Transmitter.hpp"
+#include "Receiver.hpp"
+
+#endif /* Scene_hpp */
